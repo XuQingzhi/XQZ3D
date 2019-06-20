08b626bfc534cd208ffb764b48b2b9e0746544af
lyj00912 <3122742760@qq.com>
2019-06-20 10:39:13 +0800

    优化代码，提高可视化程度

--- XQZ3D.js
+++ XQZ3D.js
@@ -2,20 +2,10 @@
 //Open Source 3D Engine
 //Project link:https://github.com/xuqingzhi/xqz3d
 
+
+
 /*
-<style>
-        html,body{
-            height:100%;
-            border:0;
-            margin:0;
-            padding:0;
-        }
-        #canvas{
-            position:absolute;
-            top:0;
-            left:0;
-        }
-    </style>
+**兼容性部分
 */
 
 //插入CSS规则
@@ -32,7 +22,13 @@ MetaTag.content="initial-scale=0.2, maximum-scale=0.2, minimum-scale=0.2, user-s
 MetaTag.charser="utf-8";
 HeadTag.appendChild(MetaTag);
 
-//Create Object
+
+
+/*
+**定义类和对象，构造生成器
+*/
+
+//3D顶点
 function vector4(x, y, z){
     this.x = x;
     this.y = y;
@@ -40,17 +36,20 @@ function vector4(x, y, z){
     this.w = 1;
 }
 
+//2D顶点
 function vector2(x, y) {
     this.x = x;
     this.y = y;
 }
 
+//角度
 function angle3(x, y, z){
     this.x = x;
     this.y = y;
     this.z = z;
 }
 
+//包含本地、世界、相机和屏幕位置的顶点
 function vertex(LocalPosition){
     this.LocalPosition = LocalPosition;
     this.Position = new vector4(null, null, null);
@@ -58,10 +57,12 @@ function vertex(LocalPosition){
     this.ScreenPosition = new vector2(null, null);
 }
 
+//面
 function mesh(vertex0Number, vertex1Number, vertex2Number){
     this.Vertexs = [vertex0Number, vertex1Number, vertex2Number];
 }
 
+//物体
 function obj(Vertexs, Meshs, Position, Angle){
     this.Vertexs = Vertexs;
     this.Meshs = Meshs;
@@ -69,6 +70,7 @@ function obj(Vertexs, Meshs, Position, Angle){
     this.Angle = Angle;
 }
 
+//相机
 function camara(Position, Angle, Screen, Plan, D, Scaling) {
     this.Screen = Screen;
     this.Plan = Plan;
@@ -83,12 +85,14 @@ function camara(Position, Angle, Screen, Plan, D, Scaling) {
     this.ZYXRotationMatrix = new matrix33(null, null, null, null, null, null, null, null, null);
 }
 
+//1*3矩阵
 function matrix13(a11, a12, a13){
     this.m11 = a11;
     this.m12 = a12;
     this.m13 = a13;
 }
 
+//3*3矩阵
 function matrix33(a11, a12, a13, a21, a22, a23, a31, a32, a33){
     this.m11 = a11;
     this.m12 = a12;
@@ -101,7 +105,13 @@ function matrix33(a11, a12, a13, a21, a22, a23, a31, a32, a33){
     this.m33 = a33;
 }
 
-//Convert Functions
+
+
+/*
+**定义函数
+*/
+
+//用于计算相机视角的矩阵函数
 function CamaraUpdating(camara){
     camara.XRotationMatrix = new matrix33(Math.cos(camara.Angle.y), 0, Math.sin(camara.Angle.y), 0, 1, 0, -Math.sin(camara.Angle.y), 0, Math.cos(camara.Angle.y));
     camara.YRotationMatrix = new matrix33(0,0,0,0,0,0,0,0,0);
@@ -109,6 +119,7 @@ function CamaraUpdating(camara){
     camara.ZYXRotationMatrix =  m33timesm33(m33timesm33(camara.ZRotationMatrix, camara.YRotationMatrix), camara.XRotationMatrix);
 }
 
+//将本地坐标转换为世界坐标
 function LocalToWorld(obj) {
     for (var i = 0; i < obj.length; i++) {
         for (var v = 0; v < obj[i].Vertexs.length; v++) {
@@ -117,6 +128,7 @@ function LocalToWorld(obj) {
     }
 }
 
+//将世界坐标转换成相机坐标
 function WorldToCamara(obj, camara) {
     for (var i = 0; i < obj.length; i++) {
         for (var v = 0; v < obj[i].Vertexs.length; v++) {
@@ -127,6 +139,7 @@ function WorldToCamara(obj, camara) {
     }
 }
 
+//将相机坐标转换成屏幕坐标，即透视投影
 function PerspectiveProjection(obj, camara) {
     for (var i = 0; i < obj.length; i++) {
         for (var v = 0; v < obj[i].Vertexs.length; v++) {
@@ -141,6 +154,7 @@ function PerspectiveProjection(obj, camara) {
     }
 }
 
+//绘制物体表面，即填色函数
 function PrintObjectMesh(obj, ctx, ctxWidth, ctxHeight){
     for (var i = 0; i < obj.length; i++) {
         for (var m = 0; m < obj[i].Meshs.length; m++) {
@@ -180,6 +194,7 @@ function PrintObjectMesh(obj, ctx, ctxWidth, ctxHeight){
     }
 }
 
+//1*3矩阵乘3*3矩阵
 function m13timesm33(m13, m33){
     var r11 = m13.m11 * m33.m11 + m13.m12 * m33.m21 + m13.m13 * m33.m31;
     var r12 = m13.m11 * m33.m12 + m13.m12 * m33.m22 + m13.m13 * m33.m32;
@@ -187,6 +202,7 @@ function m13timesm33(m13, m33){
     return new matrix13(r11, r12, r13);
 }
 
+//3*3矩阵乘3*3矩阵
 function m33timesm33(a33, b33){
     var r11 = a33.m11 * b33.m11 + a33.m12 * b33.m21 + a33.m13 * b33.m31;
     var r12 = a33.m11 * b33.m12 + a33.m12 * b33.m22 + a33.m13 * b33.m32;
